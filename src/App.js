85219dd5c890e2c4fcb9978c8ee30514843dea7e
jstreich13 <jstreich13@gmail.com>
2022-06-28 18:40:39 -0500

    Note page functioning except for the search

--- src/App.js
+++ src/App.js
@@ -5,57 +5,9 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import { useState, useEffect } from "react";
 import NotebookPage from "./pages/NotebookPage/NotebookPage";
-const uuid = require("uuid");
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: { uuid },
-      text: "This is the first note",
-      date: "7/2/2021",
-    },
-    {
-      id: { uuid },
-      text: "This is the second note",
-      date: "7/3/2021",
-    },
-    {
-      id: { uuid },
-      text: "This is the third note",
-      date: "7/4/2021",
-    },
-  ]);
-
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: { uuid },
-      text: text,
-      date: date.toLocaleDateString(),
-    };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  };
-
-  const [searchText, setSearchText] = useState("");
-
   const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
-    if (savedNotes) {
-      setNotes(savedNotes);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
-  }, [notes]);
-
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
 
   return (
     <div className="app">
@@ -79,13 +31,13 @@ const App = () => {
               <div className={`${darkMode && "dark-mode"}`}>
                 <div className="container">
                   <NotebookPage
-                    notes={notes.filter((note) =>
-                      note.text.toLowerCase().includes(searchText)
-                    )}
+                    // notes={notes.filter((note) =>
+                    //   note.text.toLowerCase().includes(searchText)
+                    // )}
                     handleToggleDarkMode={setDarkMode}
-                    handleSearchNote={setSearchText}
-                    handleAddNote={addNote}
-                    handleDelete={deleteNote}
+                    // handleSearchNote={setSearchText}
+                    // handleAddNote={addNote}
+                    // handleDelete={deleteNote}
                     {...routerProps}
                   />
                 </div>
--- src/pages/NotebookPage/NotebookPage.jsx
+++ src/pages/NotebookPage/NotebookPage.jsx
@@ -2,99 +2,138 @@ import "../../pages/NotebookPage/NotebookPage.scss";
 import { Component } from "react";
 import { MdSearch, MdDeleteForever } from "react-icons/md";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const NotebookPage = ({
   handleAddNote,
   handleSearchNote,
   handleToggleDarkMode,
-  handleDeleteNote,
-  notes,
-  setNotes,
   id,
   text,
   date,
-  noteText,
-  setNoteText,
 }) => {
-  const addNote = ({ handleAddNote, notes, setNotes }) => {
-    const [noteText, setNoteText] = useState("");
-    // const [notesList, addNote] = useState([]);
-    const characterLimit = 10000;
+  const [notes, setNotes] = useState([
+    {
+      id: uuidv4(),
+      text: "This is the first note",
+      date: "7/2/2021",
+    },
+    {
+      id: uuidv4(),
+      text: "This is the second note",
+      date: "7/3/2021",
+    },
+    {
+      id: uuidv4(),
+      text: "This is the third note",
+      date: "7/4/2021",
+    },
+  ]);
 
-    const handleChange = (event) => {
-      if (characterLimit - event.target.value.length >= 0) {
-        setNoteText(event.target.value);
-      }
-    };
+  // const addNote = (text) => {
+  //   const date = new Date();
+  //   const newNotes = [...notes, newNote];
+  //   setNotes(newNotes);
+  // };
 
-    const handleSaveClick = () => {
-      if (noteText.trim().length > 0) {
-        handleAddNote(noteText);
-        setNoteText("");
-      }
-      //create function so if noteText.trim().length == 0 then add bookmark of timestamp
-    };
+  const [searchText, setSearchText] = useState("");
 
-    //state with empty array of notes
-    //on save the state changes and the note persists, note added into the array
-    return (
-      <>
-        <div className="header">
-          <h1>Notebook</h1>
-          <button
-            onClick={() =>
-              handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
-            }
-            className="toggle"
-          >
-            Dark Mode
-          </button>
-        </div>
-        <div className="search">
-          <MdSearch className="search-icons" size="1.3em" />
-          <input
-            onChange={(event) => handleSearchNote(event.target.value)}
-            type="text"
-            placeholder="Search notes..."
-          />
-        </div>
-        <div className="notebook">
-          {/* {notes?.map((note) => ( */}
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
+
+  const handleDeleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  const [noteText, setNoteText] = useState("");
+  // const [notes, setNotes] = useState([]);
+  const characterLimit = 10000;
+
+  // const addNote = ({ handleAddNote, notes, setNotes }) => {};
+  const handleChange = (event) => {
+    if (characterLimit - event.target.value.length >= 0) {
+      setNoteText(event.target.value);
+    }
+  };
+
+  const handleSaveClick = () => {
+    if (noteText.trim().length > 0) {
+      const newNote = {
+        id: uuidv4(),
+        text: noteText,
+        date: new Date().toLocaleDateString(),
+      };
+      console.log("text");
+      setNotes([...notes, newNote]);
+      // setNoteText("");
+    }
+    //create function so if noteText.trim().length == 0 then add bookmark of timestamp
+  };
+
+  //state with empty array of notes
+  //on save the state changes and the note persists, note added into the array
+  return (
+    <>
+      <div className="header">
+        <h1>Notebook</h1>
+        <button
+          onClick={() =>
+            handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+          }
+          className="toggle"
+        >
+          Dark Mode
+        </button>
+      </div>
+      <div className="search">
+        <MdSearch className="search-icons" size="1.3em" />
+        <input
+          onChange={(event) => handleSearchNote(event.target.value)}
+          type="text"
+          placeholder="Search notes..."
+        />
+      </div>
+      <div className="notebook">
+        {notes?.map((note) => (
           <div className="note">
-            <span>{text}</span>
+            <span>{note.text}</span>
             <div className="note-footer">
-              <small> {date} </small>
+              <small> {note.date} </small>
               <MdDeleteForever
-                onClick={() => handleDeleteNote(id)}
+                onClick={() => handleDeleteNote(note.id)}
                 className="delete-icon"
                 size="1.3em"
               />
             </div>
           </div>
-          <div className="note new">
-            <textarea
-              rows="8"
-              cols="10"
-              placeholder="Add your note here"
-              value={noteText}
-              onChange={handleChange}
-            ></textarea>
-            <div className="note-footer">
-              <small>
-                {characterLimit - noteText.length} letters remaining
-              </small>
-              <button
-                className="save"
-                onClick={(handleSaveClick, handleAddNote)}
-              >
-                Save
-              </button>
-            </div>
+        ))}
+        <div className="note new">
+          <textarea
+            rows="8"
+            cols="10"
+            placeholder="Add your note here"
+            value={noteText}
+            onChange={handleChange}
+          ></textarea>
+          <div className="note-footer">
+            <small>{characterLimit - noteText.length} letters remaining</small>
+            <button className="save" onClick={handleSaveClick}>
+              Save
+            </button>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 };
 
 export default NotebookPage;
